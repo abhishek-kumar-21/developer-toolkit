04f203fea98acb527053e878cefb57b085d1ce87
Abhishek Kumar <abhishek.kumar52408@gmail.com>
2025-03-16 01:26:31 +0530

    not-found.js updated

--- app/not-found.js
+++ app/not-found.js
@@ -5,13 +5,15 @@ export default function NotFound() {
         <div className='text-white bg-slate-800 flex items-center justify-center min-h-screen flex-col'>
             <h1 className='text-8xl font-extrabold text-blue-700'>404</h1>
 
-            <p className='mt-4 text-xl font-medium text-gray-200'>Oops! The page you're looking for doesn't exist.</p>
+            <p className="mt-4 text-xl font-medium text-gray-200">
+                Oops! The page you&apos;re looking for doesn&apos;t exist.
+            </p>
 
-            <p className='my-3 text-gray-400'>
+            <p className="my-3 text-gray-400">
                 It looks like you may have taken a wrong turn.
             </p>
 
-            <Link href="/" className='rounded-lg border-none px-6 py-3 font-medium bg-blue-500 text-white hover:bg-blue-700 hover:cursor-pointer'>
+            <Link href={"/"} className='rounded-lg border-none px-6 py-3 font-medium bg-blue-500 text-white hover:bg-blue-700 hover:cursor-pointer'>
                 Return Home
             </Link>
         </div>
